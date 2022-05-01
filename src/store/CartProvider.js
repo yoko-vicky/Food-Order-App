e3fd1d970729d5f7c5fb6d210616a43de7d41422
Yoko Saka <yoko.saka@tora.com>
2022-05-02 08:44:01 +0900

    Add CartReducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,60 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM_TO_CART":
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    case "REMOVE_ITEM_FROM_CART":
+      const updatedAmount = state.items.reduce((total, item) => {
+        if (item.id === action.id) {
+          return total - item.amount;
+        }
+      }, state.totalAmount);
+
+      return {
+        items: state.items.filter((item) => item.id !== action.id),
+        totalAmount: updatedAmount,
+      };
+    default:
+      return defaultCartState;
+  }
+};
+
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({
+      type: "ADD_ITEM_TO_CART",
+      item: item,
+    });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({
+      type: "REMOVE_ITEM_FROM_CART",
+      id: id,
+    });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
